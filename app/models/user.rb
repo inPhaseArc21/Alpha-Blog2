a946d4b558c041aaa401c20f42d0467c4706039a
Raj Silari <raj.silari@icloud.com>
2016-01-14 12:03:06 +1000

    Add secure password for a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
 has_many :articles
 
+
 before_save { self.email = email.downcase }
 
 	validates :username, presence: true, 
@@ -13,5 +14,8 @@ before_save { self.email = email.downcase }
 	validates :email, presence: true, length: { maximum: 105 },
 			  uniqueness: { case_sensitive: false },
 			  format: { with: VALID_EMAIL_REGEX }
+
+
+has_secure_password
 
 end
