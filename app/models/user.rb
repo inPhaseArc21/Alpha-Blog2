6abaa1fc2312a3f17e692f3f4b3dafb91c229776
Raj Silari <raj.silari@icloud.com>
2016-01-14 20:08:12 +1000

    Sign up users from UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ before_save { self.email = email.downcase }
 			  uniqueness: { case_sensitive: false },
 			  format: { with: VALID_EMAIL_REGEX }
 
-
+			  has_secure_password
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114015728) do
+ActiveRecord::Schema.define(version: 20160114034836) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
