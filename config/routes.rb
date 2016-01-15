92560d6be9607ba684b2c23d816f90f594b06b61
Raj Silari <raj.silari@icloud.com>
2016-01-15 11:58:57 +1000

    Create edit users page and form partials

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
 
+  resources :users, except: [:new]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
