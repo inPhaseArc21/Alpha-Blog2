07bed35aa5f1f71d2675806716b6bfa535f0cff9
Raj Silari <raj.silari@icloud.com>
2016-01-28 20:24:15 +1000

    Update instance variables

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 
 	def create  
 	  @article = Article.new(article_params)
-	  @article.user = User.first 
+	  @article.user = User.find(10)
 	   if @article.save
 	      flash[:success] = "Article was successfully created"
 	      redirect_to article_path(@article)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
 
 
 	def create
-	  @user = User.new(user_params)
-	    if @user.save
-	     flash[:success] = "Welcome to Alpha Blog #{@user.username}"
+	  @users = User.new(user_params)
+	    if @users.save
+	     flash[:success] = "Welcome to Alpha Blog #{@users.username}"
 	     redirect_to articles_path
 		else
          render 'new'
@@ -19,12 +19,12 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-	  @user = User.find(params[:id])
+	  @users = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
-		if @user.update(user_params)
+		@users = User.find(params[:id])
+		if @users.update(user_params)
   	  	  flash[:success] = "Your account was successfully updated"
   	  	  redirect_to articles_path
   	  	 else
@@ -33,14 +33,8 @@ class UsersController < ApplicationController
     end
 
     def show 
-    	@user = User.find(params[:id])
+    	@users = User.find(params[:id])
     end
-
-  
-
-
-
-
 
 	private 
 	def user_params
