6179641dc120d56e4ac075130ff7d178c3dd8dea
Raj Silari <raj.silari@icloud.com>
2016-02-10 12:09:24 +1000

    Update all views with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,8 +24,8 @@ before_action :require_admin, except: [:index, :show]
 
 
 	def show
-		@category = Category.new
-		@category2 = Category.new
+		@category = Category.find(params[:id])
+		@category_articles = @category.articles.paginate(page:params[:page], per_page: 5)
 	end
 
 	private
